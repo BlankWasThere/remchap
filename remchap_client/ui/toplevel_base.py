0366c00e15a005395c00537c0e6f02e09a793598
BlankWasThere <186826343+BlankWasThere@users.noreply.github.com>
2025-06-02 21:45:17 +0530

    fix(client connection window): double `on_close` callback invocation

--- remchap_client/ui/toplevel_base.py
+++ remchap_client/ui/toplevel_base.py
@@ -5,6 +5,7 @@ import customtkinter
 
 class TopLevelBase(customtkinter.CTkToplevel):
     def on_close(self, cb: Callable):
-        self.protocol("WM_DELETE_WINDOW", cb)
+        ## ERROR: The below line causes double callback invocation
+        # self.protocol("WM_DELETE_WINDOW", cb)
 
         self.bind("<Destroy>", lambda e: cb() if e.widget == self else None)
